d9fe63c2b74e1c443132ec996dc6e0e21705b568
Hetalbharadiya <124438193+Hetalbharadiya@users.noreply.github.com>
2023-02-14 16:55:19 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,8 @@
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.3/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.3/css/bootstrap.min.css"/>
+
+
+<?php
+     $con = mysqli_connect("localhost","root","","school_mgt");
+     
+?>
